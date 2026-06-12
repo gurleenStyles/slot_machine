2adf6eb5ad13488bf3f40d0984bc185bd197bcbd
Gurleen <smartgurleen5@gmail.com>
2026-06-12 11:59:25 +0530

    made deposit,get number of lines, get bet

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#okay so first i creates a repo in my local machine and to add this on my github i have to  create a repo with no readme file (this is imp)then git init git git remote add origin <url> git add . git commit -m "first commit" git push -u origin main
+MAX_LINES=3 #constant value that won't change in the program
 def deposit():
     while True:#we are asking from the user until they give us a valid input
 
@@ -8,7 +10,26 @@ def deposit():
             if amount>0:
                 break #while will run untill this break is reached
             else:
-                print("amount must be grater than 0")
+                print("amount must be grater than 0")#never executed because of the isdigit() check
         else:
-            print("please enter a valid number")
-        return amount
+            print("please enter a number")
+    return amount
+
+def get_number_of_lines():
+    while True:
+        lines= input("enter the number of lines to bet on(1-"+ str(MAX_LINES)+ ")? ")
+        if lines.isdigit():
+            lines =int(lines)
+            if 1<= lines <= MAX_LINES:
+                break
+            else:
+                print("enter a valid number of lines")
+    return lines
+
+
+def main():
+  balance = deposit()
+  lines =get_number_of_lines()
+  print (balance,lines)
+
+main()
