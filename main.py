7b518e4b7644eede2ecf4c6a16040b1dfecc3a86
Gurleen <smartgurleen5@gmail.com>
2026-06-12 18:05:25 +0530

    ayay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #okay so first i creates a repo in my local machine and to add this on my github i have to  create a repo with no readme file (this is imp)then git init git git remote add origin <url> git add . git commit -m "first commit" git push -u origin main
-MAX_LINES=3 #constant value that won't change in the program
+MAX_LINES=3 
+MAX_BET= 100
+MIN_BET =1 #constant value that won't change in the program
 def deposit():
     while True:#we are asking from the user until they give us a valid input
 
@@ -26,10 +28,31 @@ def get_number_of_lines():
                 print("enter a valid number of lines")
     return lines
 
+def get_bet():
+    while True:
+        amount = input("what would you like to bet on each line?")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"amount must be between ${MIN_BET} and ${MAX_BET}.")
+        else:
+            print("please enter a number")
+    return amount
 
 def main():
   balance = deposit()
   lines =get_number_of_lines()
-  print (balance,lines)
+  while True:
+    bet= get_bet()
+    total_bet = bet* lines
+    if total_bet>balance:
+        print(f"you don't have enough to bet that amount(${total_bet}), your current blance is: ${balance}")
+    else:
+        break
+  print(f"you are betting to ${bet} on {lines}. total bet is equal to ${total_bet}")
+  print (balance,bet)
+
 
 main()
